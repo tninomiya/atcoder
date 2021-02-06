f31fc1df397fe3df53e3a31f38da69862b742333
Tomoki Ninomiya <t.miliya612@gmail.com>
2021-02-06 17:02:49 +0900

    solve abc088b

---
+++ abc088/abc088-b/src/main.rs
@@ -0,0 +1,21 @@
+// -*- coding:utf-8-unix -*-
+
+use proconio::input;
+
+fn main() {
+    input! {
+        n: usize,
+        mut cards: [u32; n],
+    }
+    cards.sort();
+    let mut alice = 0;
+    let mut bob = 0;
+    for (i, v) in cards.iter().rev().enumerate() {
+        if i % 2 == 0 {
+            alice += v;
+        } else {
+            bob += v;
+        }
+    }
+    println!("{}", alice - bob);
+}
