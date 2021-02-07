c169c71ade0b7eab547c4c3c7451c18bfdd440f4
Tomoki Ninomiya <t.miliya612@gmail.com>
2021-02-07 14:24:02 +0900

    tuning abc087b

--- abs/src/bin/abc087b.rs
+++ abs/src/bin/abc087b.rs
@@ -8,18 +8,17 @@ fn main() {
         x: usize,
     }
     let mut res = 0;
-    for i in (0..=a).rev() {
+    for i in 0..=a {
         if i * 500 > x {
-            continue;
+            break;
         }
-        for j in (0..=b).rev() {
+        for j in 0..=b {
             if i * 500 + j * 100 > x {
-                continue;
+                break;
             }
-            for k in (0..=c).rev() {
-                if i * 500 + j * 100 + k * 50 == x {
-                    res += 1;
-                }
+            let remaining = x - (i * 500 + j * 100);
+            if remaining % 50 == 0 && remaining / 50 <= c {
+                res += 1;
             }
         }
     }
