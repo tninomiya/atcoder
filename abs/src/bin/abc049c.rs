be76d83997aafc85b5e64e48787079202e5c5e5e
Tomoki Ninomiya <t.miliya612@gmail.com>
2021-02-07 15:01:13 +0900

    solve abc049c

--- abs/src/bin/abc049c.rs
+++ abs/src/bin/abc049c.rs
@@ -1,3 +1,25 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        s: String,
+    }
+    let mut s: &str = &s;
+    loop {
+        let l = s.len();
+        if s.ends_with("dreamer") {
+            s = &s[..l - 7];
+        } else if s.ends_with("eraser") {
+            s = &s[..l - 6];
+        } else if s.ends_with("dream") || s.ends_with("erase") {
+            s = &s[..l - 5];
+        } else {
+            break;
+        }
+    }
+    if s.len() == 0 {
+        println!("YES");
+    } else {
+        println!("NO");
+    }
 }
